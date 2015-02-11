42a3f62e178e7a0ad22adad4be683e70a58007e4
Bernd Fecht (encad-consulting.de) <fechbern@Admins-MacBook-Pro.local>
2015-02-11 08:55:32 +0100

    Initial Commit

---
+++ encadApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  encadApp
+//
+//  Created by Bernd Fecht (encad-consulting.de) on 11.02.15.
+//  Copyright (c) 2015 Bernd Fecht (encad-consulting.de). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ encadApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  encadApp
+//
+//  Created by Bernd Fecht (encad-consulting.de) on 11.02.15.
+//  Copyright (c) 2015 Bernd Fecht (encad-consulting.de). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
